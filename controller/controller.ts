4490f875e0f177dd3028275aca94c59e948936f0
LorenzoLongarini <s1089145@studenti.univpm.it>
2023-06-16 16:21:51 +0200

    fix get tokens and update

--- controller/controller.ts
+++ controller/controller.ts
@@ -12,15 +12,6 @@ export const getUserTokens = (req: any, res: any) => {
     return getTokens(req, res);
 };
 
-// export const updateCredits = (req: any, res: any) => {
-//     let user = await findUser(req.body.email)
-//     updateUserCredits(req.body.credits, user[0].dataValues.id)
-//         .then(() => res.status(StatusCodes.OK).json(Utils.getReasonPhrase(StatusCodes.OK, successMsg.UPDATE_CREDITS)))
-//         .catch((err) => res.status(StatusCodes.BAD_REQUEST).json(Utils.getReasonPhrase(StatusCodes.BAD_REQUEST, exceptionMsg.ERR_UPDATE_CREDITS + err)))
-
-// }
-
-
 export const createUser = (req: any, res: any) => {
     return createUserService(req, res);
 }
