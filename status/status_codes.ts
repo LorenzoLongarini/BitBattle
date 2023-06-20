e5a48c8bcce94462c4e71bc16201ccb5f4260e23
LorenzoLongarini <s1089145@studenti.univpm.it>
2023-06-20 17:17:56 +0200

    create new game ok

--- status/status_codes.ts
+++ status/status_codes.ts
@@ -11,6 +11,8 @@ export enum Messages300 {
 }
 export enum Messages400 {
     UnauthorizedUser = "Non è possibile creare l\'utente perchè è già esistente",
+    OutOfBoundGrid = "La dimensione della griglia deve essere compreso tra 3 e 10",
+    OutOfBoundShips = "Il numero di navi inserite è troppo alto",
 }
 
 export enum Messages500 {
