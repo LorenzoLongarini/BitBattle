8f2825a68ca549662adf2d59e8a821bc1d520373
LorenzoLongarini <s1089145@studenti.univpm.it>
2023-06-21 16:58:26 +0200

    change logica del random

--- sql/schema.sql
+++ sql/schema.sql
@@ -13,7 +13,8 @@
 
  CREATE TABLE game(
       game_id SERIAL PRIMARY KEY,
-      players JSON,
+      player1 VARCHAR(25),
+      player2 VARCHAR(25),
       name VARCHAR(15),
       grid_size INTEGER,
       ships JSON,
@@ -30,12 +31,12 @@ INSERT INTO users (email, password, tokens, isadmin, isplaying, points) VALUES
  ('loris@bitbattle.it','bitbattle',10.00,true,false, 13),
  ('lorenzo@bitbattle.it','bitbattle1',10.00,false,false, 26);
 
-INSERT INTO game (players, name, grid_size, ships, possible_moves,  moves, status, winner, score) VALUES
- ('[{"player1": "loris@bitbattle.it"}, {"player2": "lorenzo@bitbattle.it"}]', 'game_1',  2, '[{"size1": 3}, {"size2": 2 }, {"size3": 3}]',
- '[{"move": [1, 1], "ship": false}, {"move": [1, 2], "ship": true}, {"move": [2, 1], "ship": false}, {"move": [2, 2], "ship": true}]',   
+INSERT INTO game (player1, player2, name, grid_size, ships, possible_moves,  moves, status, winner, score) VALUES
+ ('loris@bitbattle.it','', 'game_1',  2, '[{"size1": 3}, {"size2": 2 }, {"size3": 3}]',
+ '[{"move": [1, 1], "ship": 0, "owner":""}, {"move": [1, 2], "ship": 1, "owner":"loris@bitbattle.it"}, {"move": [2, 1], "ship": 0, "owner":""}, {"move": [2, 2], "ship": 1, "owner":"lorenzo@bitbattle.it"}]',   
  '[{"move": [1, 1], "hit": false, "player": "loris@bitbattle.it"}, {"move": [1, 2], "hit": true, "player": "lorenzo@bitbattle.it"}, {"move": [2, 1], "hit": false, "player": "loris@bitbattle.it"}]', 
  'started', 'lorenzo@bitbattle.it', '[{"loris@bitbattle.it": 1, "lorenzo@bitbattle.it": 15}]' ),
- ('[{"player1": "loris@bitbattle.it"}, {"player2": "lorenzo@bitbattle.it"}]','game_2',  2, '[{"size1": 3}, {"size2": 2 }, {"size3": 3}]',
- '[{"move": [1, 1], "ship": false}, {"move": [1, 2], "ship": true}, {"move": [2, 1], "ship": false}, {"move": [2, 2], "ship": true}]',   
+ ('loris@bitbattle.it','','game_2',  2, '[{"size1": 3}, {"size2": 2 }, {"size3": 3}]',
+ '[{"move": [1, 1], "ship": 0, "owner":""}, {"move": [1, 2], "ship": 1, "owner":"loris@bitbattle.it"}, {"move": [2, 1], "ship": 0, "owner":""}, {"move": [2, 2], "ship": 1, "owner":"lorenzo@bitbattle.it"}]',   
  '[{"move": [2, 1], "hit": false, "player": "lorenzo@bitbattle.it"}, {"move": [1, 2], "hit": true, "player": "loris@bitbattle.it"}]', 
  'finished', 'loris@bitbattle.it', '[{"loris@bitbattle.it": 12, "lorenzo@bitbattle.it": 11}]' );
--- utils/game_utils.ts
+++ utils/game_utils.ts
@@ -4,7 +4,7 @@ export function getAllMoves(gridSize: number): any {
     let possibleMoves: any = [];
     for (let i = 1; i <= gridSize; i++) {
         for (let j = 1; j <= gridSize; j++) {
-            possibleMoves.push({ move: [i, j], ship: false });
+            possibleMoves.push({ move: [i, j], ship: 0, owner: "" });
         }
     }
     return possibleMoves;
@@ -14,104 +14,164 @@ export function setShips(gridSize: number, req: Request): any {
 
     let grid = getAllMoves(gridSize);
 
+    let player1 = req.body.player1;
+    let player2 = req.body.player2;
+
+    let totalLength;
+
+    let allPlayers = [req.body.email];
+
+    if (player1 !== "" && player2 !== "") {
+        totalLength = 3;
+        allPlayers.push(player1, player2);
+    } else if (player1 !== "" && player2 === "") {
+        totalLength = 2;
+        allPlayers.push(player1);
+    } else if (player1 === "" && player2 !== "") {
+        totalLength = 2;
+        allPlayers.push(player2);
+    } else {
+        totalLength = 2;
+        allPlayers.push("AI");
+    }
+
+
     let size1Ships = req.body.ships[0].size1;
-    let size2Ships = req.body.ships[1].size2
-    let size3Ships = req.body.ships[2].size3
+
+    let size2Ships = req.body.ships[1].size2;
+
+    let size3Ships = req.body.ships[2].size3;
+
+    for (let i in allPlayers) {
+        grid = populateGrid(grid, size1Ships, gridSize, 1, allPlayers[i]);
+        grid = populateGrid(grid, size2Ships, gridSize, 2, allPlayers[i]);
+        grid = populateGrid(grid, size3Ships, gridSize, 3, allPlayers[i]);
+    }
 
 
-    grid = populateGrid(grid, size1Ships, gridSize, 1);
-    grid = populateGrid(grid, size2Ships, gridSize, 2);
-    grid = populateGrid(grid, size3Ships, gridSize, 3);
+
     return grid;
 }
 
 
-function populateGrid(grid: any, size: number, gridSize: number, shipType: number) {
+function populateGrid(grid: any, size: number, gridSize: number, shipType: number, owner: string) {
     let gridDimension = gridSize * gridSize;
 
     for (let i = 0; i < size; i++) {
         let startIndex: number = Math.floor(Math.random() * gridDimension);
 
 
-        let currentPosEmpty = !grid[startIndex].ship;
+        let currentPosEmpty = grid[startIndex].ship == 0;
 
-        let xPosEmptySucc1 = (startIndex + gridSize < gridDimension) ? !grid[startIndex + gridSize].ship : false;
-        let xPosEmptySucc2 = (startIndex + gridSize + gridSize < gridDimension) ? !grid[startIndex + gridSize + gridSize].ship : false;
+        let xPosEmptySucc1 = (startIndex + gridSize < gridDimension) ? grid[startIndex + gridSize].ship == 0 : 0;
+        let xPosEmptySucc2 = (startIndex + gridSize + gridSize < gridDimension) ? grid[startIndex + gridSize + gridSize].ship == 0 : 0;
 
-        let xPosEmptyPrev1 = (startIndex - gridSize >= 0) ? !grid[startIndex - gridSize].ship : false;
-        let xPosEmptyPrev2 = (startIndex - gridSize - gridSize >= 0) ? !grid[startIndex - gridSize - gridSize].ship : false;
+        let xPosEmptyPrev1 = (startIndex - gridSize >= 0) ? grid[startIndex - gridSize].ship == 0 : 0;
+        let xPosEmptyPrev2 = (startIndex - gridSize - gridSize >= 0) ? grid[startIndex - gridSize - gridSize].ship == 0 : 0;
 
-        let yPosEmptySucc1 = (startIndex + 1 < gridDimension) ? !grid[startIndex + 1].ship : false;
-        let yPosEmptySucc2 = (startIndex + 2 < gridDimension) ? !grid[startIndex + 2].ship : false;
+        let yPosEmptySucc1 = (startIndex + 1 < gridDimension) ? grid[startIndex + 1].ship == 0 : 0;
+        let yPosEmptySucc2 = (startIndex + 2 < gridDimension) ? grid[startIndex + 2].ship == 0 : 0;
 
-        let yPosEmptyPrev1 = (startIndex - 1 >= 0) ? !grid[startIndex - 1].ship : false;
-        let yPosEmptyPrev2 = (startIndex - 2 >= 0) ? !grid[startIndex - 2].ship : false;
+        let yPosEmptyPrev1 = (startIndex - 1 >= 0) ? grid[startIndex - 1].ship == 0 : 0;
+        let yPosEmptyPrev2 = (startIndex - 2 >= 0) ? grid[startIndex - 2].ship == 0 : 0;
 
 
         while (!currentPosEmpty) {
             if (xPosEmptySucc1) {
                 startIndex += 1;
-            } else if (xPosEmptyPrev1) {
-                startIndex += 1;
             } else if (yPosEmptySucc1) {
                 startIndex += 1;
+            } else if (xPosEmptyPrev1) {
+                startIndex -= 1;
             } else if (yPosEmptyPrev2) {
                 startIndex -= 1;
             } else {
                 break;
             }
             currentPosEmpty = !grid[startIndex].ship;
-            xPosEmptySucc1 = (startIndex + gridSize < gridDimension) ? !grid[startIndex + gridSize].ship : false;
-            xPosEmptySucc2 = (startIndex + gridSize + gridSize < gridDimension) ? !grid[startIndex + gridSize + gridSize].ship : false;
-            xPosEmptyPrev1 = (startIndex - gridSize >= 0) ? !grid[startIndex - gridSize].ship : false;
-            xPosEmptyPrev2 = (startIndex - gridSize - gridSize >= 0) ? !grid[startIndex - gridSize - gridSize].ship : false;
-            yPosEmptySucc1 = (startIndex + 1 < gridDimension) ? !grid[startIndex + 1].ship : false;
-            yPosEmptySucc2 = (startIndex + 2 < gridDimension) ? !grid[startIndex + 2].ship : false;
-            yPosEmptyPrev1 = (startIndex - 1 >= 0) ? !grid[startIndex - 1].ship : false;
-            yPosEmptyPrev2 = (startIndex - 2 >= 0) ? !grid[startIndex - 2].ship : false;
+            xPosEmptySucc1 = (startIndex + gridSize < gridDimension) ? grid[startIndex + gridSize].ship == 0 : 0;
+            xPosEmptySucc2 = (startIndex + gridSize + gridSize < gridDimension) ? grid[startIndex + gridSize + gridSize].ship == 0 : 0;
+            xPosEmptyPrev1 = (startIndex - gridSize >= 0) ? grid[startIndex - gridSize].ship == 0 : 0;
+            xPosEmptyPrev2 = (startIndex - gridSize - gridSize >= 0) ? grid[startIndex - gridSize - gridSize].ship == 0 : 0;
+            yPosEmptySucc1 = (startIndex + 1 < gridDimension) ? grid[startIndex + 1].ship == 0 : 0;
+            yPosEmptySucc2 = (startIndex + 2 < gridDimension) ? grid[startIndex + 2].ship == 0 : 0;
+            yPosEmptyPrev1 = (startIndex - 1 >= 0) ? grid[startIndex - 1].ship == 0 : 0;
+            yPosEmptyPrev2 = (startIndex - 2 >= 0) ? grid[startIndex - 2].ship == 0 : 0;
 
         }
-        let shiptype2 = shipType == 2;
-        if (!(shipType == 3)) {
-            if (currentPosEmpty && xPosEmptySucc1) {
-                grid[startIndex].ship = true;
-                (shiptype2) ? grid[startIndex + gridSize].ship = true : null;
+
+        if (shipType == 1) {
+            if (currentPosEmpty) {
+                grid[startIndex].ship = 1;
+                grid[startIndex].owner = owner;
+            }
+        }
+        else if (shipType == 2) {
+            if (currentPosEmpty && xPosEmptySucc1 && xPosEmptyPrev1 && xPosEmptySucc2) {
+                grid[startIndex].ship = 2;
+                grid[startIndex + gridSize].ship = 2;
+                grid[startIndex].owner = owner;
+                grid[startIndex + gridSize].owner = owner;
             } else if (currentPosEmpty && yPosEmptySucc1) {
-                grid[startIndex].ship = true;
-                (shiptype2) ? grid[startIndex + 1].ship = true : null;
+                grid[startIndex].ship = 2;
+                grid[startIndex + 1].ship = 2;
+                grid[startIndex].owner = owner;
+                grid[startIndex + 1].owner = owner;
             } else if (currentPosEmpty && xPosEmptyPrev1) {
-                grid[startIndex].ship = true;
-                (shiptype2) ? grid[startIndex - 1].ship = true : null;
+                grid[startIndex].ship = 2;
+                grid[startIndex - 1].ship = 2;
+                grid[startIndex].owner = owner;
+                grid[startIndex - 1].owner = owner;
             } else if (currentPosEmpty && yPosEmptyPrev1) {
-                grid[startIndex].ship = true;
-                (shiptype2) ? grid[startIndex - 1].ship = true : null;
+                grid[startIndex].ship = 2;
+                grid[startIndex - 1].ship = 2;
+                grid[startIndex].owner = owner;
+                grid[startIndex - 1].owner = owner;
             }
 
         } else {
             if (currentPosEmpty && xPosEmptySucc1 && xPosEmptyPrev1) {
-                grid[startIndex].ship = true;
-                grid[startIndex + gridSize].ship = true;
-                grid[startIndex - gridSize].ship = true;
+                grid[startIndex].ship = 3;
+                grid[startIndex + gridSize].ship = 3;
+                grid[startIndex - gridSize].ship = 3;
+                grid[startIndex].owner = owner;
+                grid[startIndex + gridSize].owner = owner;
+                grid[startIndex - gridSize].owner = owner;
             } else if (currentPosEmpty && yPosEmptySucc1 && yPosEmptyPrev1) {
-                grid[startIndex].ship = true;
-                grid[startIndex + 1].ship = true;
-                grid[startIndex - 1].ship = true;
+                grid[startIndex].ship = 3;
+                grid[startIndex + 1].ship = 3;
+                grid[startIndex - 1].ship = 3;
+                grid[startIndex].owner = owner;
+                grid[startIndex + 1].owner = owner;
+                grid[startIndex - 1].owner = owner;
             } else if (currentPosEmpty && xPosEmptySucc1 && xPosEmptySucc2) {
-                grid[startIndex].ship = true;
-                grid[startIndex + 1].ship = true;
-                grid[startIndex + 2].ship = true;
+                grid[startIndex].ship = 3;
+                grid[startIndex + 1].ship = 3;
+                grid[startIndex + 2].ship = 3;
+                grid[startIndex].owner = owner;
+                grid[startIndex + 1].owner = owner;
+                grid[startIndex + 2].owner = owner;
             } else if (currentPosEmpty && xPosEmptyPrev1 && xPosEmptyPrev2) {
-                grid[startIndex].ship = true;
-                grid[startIndex - 1].ship = true;
-                grid[startIndex - 2].ship = true;
+                grid[startIndex].ship = 3;
+                grid[startIndex - 1].ship = 3;
+                grid[startIndex - 2].ship = 3;
+                grid[startIndex].owner = owner;
+                grid[startIndex - 1].owner = owner;
+                grid[startIndex - 2].owner = owner;
             } else if (currentPosEmpty && yPosEmptySucc1 && yPosEmptySucc2) {
-                grid[startIndex].ship = true;
-                grid[startIndex + 1].ship = true;
-                grid[startIndex + 2].ship = true;
+                grid[startIndex].ship = 3;
+                grid[startIndex + 1].ship = 3;
+                grid[startIndex + 2].ship = 3;
+                grid[startIndex].owner = owner;
+                grid[startIndex + 1].owner = owner;
+                grid[startIndex + 2].owner = owner;
             } else if (currentPosEmpty && yPosEmptyPrev1 && yPosEmptyPrev2) {
-                grid[startIndex].ship = true;
-                grid[startIndex - 1].ship = true;
-                grid[startIndex - 2].ship = true;
+                grid[startIndex].ship = 3;
+                grid[startIndex - 1].ship = 3;
+                grid[startIndex - 2].ship = 3;
+                grid[startIndex].owner = owner;
+                grid[startIndex - 1].owner = owner;
+                grid[startIndex - 2].owner = owner;
             }
         }
     }
