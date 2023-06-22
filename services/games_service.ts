8769daa7f3c1d73d0ad10137bbf0e4690a6954ef
LorenzoLongarini <s1089145@studenti.univpm.it>
2023-06-22 09:22:22 +0200

    partita finita fix

--- services/games_service.ts
+++ services/games_service.ts
@@ -36,45 +36,50 @@ export async function doMoveService(req: Request, res: Response) {
         let hitShip = await findShip(movesPossible, targetMove, !choose);
         let currentPlayer = await findUser(req.body.player);
         let currentTokens = parseFloat(currentPlayer[0].dataValues.tokens)
+        if (searchGame[0].dataValues.status !== "finished") {
+            if (isAvailable && !isExecute && lastPlayer != player) {
 
-        if (isAvailable && !isExecute && lastPlayer != player) {
+                let reducedMovesPossible = searchGame[0].dataValues.possible_moves.filter((move: any) => (move.ship >= 1 && move.ship <= 3));
+                // let reducedMovesPossible = searchGame[0].dataValues.possible_moves.filter((move: any) => {
+                //     let shipSize: any = Object.values(move)[0];
+                //     return shipSize >= 1 && shipSize <= 3;
+                // });
+                let reducedMovesExecute;
+                // console.log(reducedMovesPossible.length, reducedMovesPossible, reducedMovesExecute.length, reducedMovesExecute)
+                let newMove = {
+                    move: targetMove,
+                    hit: hitShip,
+                    player: player
+                };
 
-            let reducedMovesPossible = searchGame[0].dataValues.possible_moves.filter((move: any) => (move.ship >= 1 && move.ship <= 3));
-            // let reducedMovesPossible = searchGame[0].dataValues.possible_moves.filter((move: any) => {
-            //     let shipSize: any = Object.values(move)[0];
-            //     return shipSize >= 1 && shipSize <= 3;
-            // });
-            let reducedMovesExecute;
-            // console.log(reducedMovesPossible.length, reducedMovesPossible, reducedMovesExecute.length, reducedMovesExecute)
-            let newMove = {
-                move: targetMove,
-                hit: hitShip,
-                player: player
-            };
-            if (searchGame[0].dataValues.status !== "finished") {
                 movesExecute.push(newMove);
                 await addMoveDb(req.body.name, movesExecute);
                 reducedMovesExecute = searchGame[0].dataValues.moves.filter((move: any) => move.hit);
                 let updatedTokens = currentTokens - 0.015;
                 await updateUserTokensDb(updatedTokens, req.body.player);
 
-            }
 
-            if (reducedMovesPossible.length == reducedMovesExecute.length) {
 
-                try {
-                    await gameOver(req.body.name);
-                    // let allPlayers =
-                    //     await findPlayer(req.body.player)
-                    // await userIsNotPlayingDb()
-                    res.json({ esito: "Game Over" });
+                if (reducedMovesPossible.length == reducedMovesExecute.length) {
 
-                } catch (err) { res.json({ errore: err }); };
-            }
-            res.json({ mossa: "Mossa eseguita" });
-        } else if (isAvailable && isExecute) {
-            res.json({ mossa: "Mossa già eseguita" });
-        } else { res.json({ mossa: "Mossa non ammissibile" }); }
+                    try {
+                        await gameOver(req.body.name);
+                        // await 
+                        // let allPlayers =
+                        //     await findPlayer(req.body.player)
+                        // await userIsNotPlayingDb()
+                        res.json({ esito: "Game Over" });
+
+                    } catch (err) { res.json({ errore: err }); };
+
+                }
+                res.json({ mossa: "Mossa eseguita" });
+            } else if (isAvailable && isExecute) {
+                res.json({ mossa: "Mossa già eseguita" });
+            } else { res.json({ mossa: "Mossa non ammissibile" }); }
+        } else {
+            res.json({ esito: "Partita finita" });
+        }
 
 
     } catch (error) {
